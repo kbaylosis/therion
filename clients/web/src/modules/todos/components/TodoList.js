56baab6fd4c96490ed80bcc626b7437e987bdba0
Elli Dustine Verano <everano@zoogtech.com>
2018-02-08 18:39:06 +0800

    added transition to todos

--- clients/web/src/modules/todos/components/TodoList.js
+++ clients/web/src/modules/todos/components/TodoList.js
@@ -6,6 +6,8 @@ import {
 
 import Todo from "./Todo";
 
+import QueueAnim from "rc-queue-anim";
+
 class TodoList extends PureComponent {
 	constructor(props) {
 		super(props);
@@ -13,27 +15,70 @@ class TodoList extends PureComponent {
 		const { onTodoClick } = props;
 
 		this.state = {
-			columns: [{
-				title: "Item",
-				dataIndex: "text",
-				key: "text",
-				render: (text, record, index) => (
-					<Todo
-						key={ record.id }
-						{...record}
-						onClick={() => onTodoClick(record.id)}
-					/>
-				),
-			}],
+			columns: [
+				{
+					title: "Queue No.",
+					dataIndex: "id",
+					key: "id",
+					render: (text, record) => (
+						<QueueAnim delay={[250, 0]} type={["top", "bottom"]} duration={[300, 300]}>
+							<li key={record.id}>{text + 1}</li>
+						</QueueAnim>
+					),
+					width: "33%",
+				},
+				{
+					title: "Item",
+					dataIndex: "text",
+					key: "text",
+					render: (text, record, index) => (
+						<QueueAnim delay={[300, 0]} type={["top", "bottom"]} duration={[300, 300]}>
+							<Todo
+								key={ record.id }
+								{...record}
+								onClick={() => onTodoClick(record.id)}
+							/>
+						</QueueAnim>
+					),
+					width: "33%",
+				},
+				{
+					title: "Status",
+					dataIndex: "completed",
+					key: "completed",
+					render: (text, record) => {
+						if (text === true) {
+							return (
+								<QueueAnim delay={[350, 0]} type={["top", "bottom"]} duration={[300, 150]}>
+									<li key={record.id}>Completed</li>
+								</QueueAnim>
+							);
+						} else if (text === false) {
+							return (
+								<QueueAnim delay={[350, 0]} type={["top", "bottom"]} duration={[300, 150]}>
+									<li key={record.id}>Active</li>
+								</QueueAnim>
+							);
+						}
+					},
+					width: "33%",
+				},
+			],
 		};
 	}
 
 	render() {
+		console.log("props", this.props, "state", this.state);
 		const { todos } = this.props;
 		const { columns } = this.state;
+		const pageSize = 10;
 
 		return (
-			<Table dataSource={ todos } columns={ columns } />
+			<Table
+				dataSource={ todos }
+				columns={ columns }
+				pagination={todos.length > pageSize && { pageSize }}
+			/>
 		);
 	}
 }
