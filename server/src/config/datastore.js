3d21667b9da3afe58a810ad4c9dfa66f7988fc25
Gomigu Inc <gomiguinc@Gomigus-iMac-5.local>
2018-07-20 19:12:38 +0800

    Add datastore environment options

--- server/src/config/datastore.js
+++ server/src/config/datastore.js
@@ -1,10 +1,28 @@
 
 const datastore = {
-	dialect: "mysql",
-	host: "localhost",
-	name: "therion",
-	username: "root",
-	password: "root",
+	development: {
+		dialect: "sqlite",
+		storage: "./db.therion.sqlite",
+		name: "therion",
+		username: null,
+		password: null,
+	},
+
+	test: {
+		dialect: "mysql",
+		host: "localhost",
+		name: "therion",
+		username: "root",
+		password: "root",
+	},
+
+	production: {
+		dialect: process.env.DB_DIALECT || "mysql",
+		host: process.env.DB_HOST ||  "localhost",
+		name: process.env.DB_NAME || "therion",
+		username: process.env.DB_USERNAME || "root",
+		password: process.env.DB_PASSWORD || "root",
+	},
 };
 
 export default datastore;
